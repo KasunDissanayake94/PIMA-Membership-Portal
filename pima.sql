71b309b5aebf223456d2352453c270a41fd2cc25
pramodaya <pramodayajayalath@gmail.com>
2019-02-15 10:25:55 +0530

    Create pima.sql
    database created

---
+++ pima.sql
@@ -0,0 +1,130 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 15, 2019 at 04:44 AM
+-- Server version: 5.7.14
+-- PHP Version: 5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pima`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `groups`
+--
+
+CREATE TABLE `groups` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `member_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `member`
+--
+
+CREATE TABLE `member` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `surname` varchar(255) NOT NULL,
+  `name_in_full` varchar(255) NOT NULL,
+  `nic_no` varchar(255) NOT NULL,
+  `passport_no` varchar(255) NOT NULL,
+  `mobile_1` varchar(255) NOT NULL,
+  `mobile_2` varchar(255) NOT NULL,
+  `personal_email` varchar(255) NOT NULL,
+  `official_email` varchar(255) NOT NULL,
+  `residence_address` varchar(255) NOT NULL,
+  `residence_address_1` varchar(255) NOT NULL,
+  `residence_address_2` varchar(255) NOT NULL,
+  `residence_city` varchar(255) NOT NULL,
+  `resicence_district` varchar(255) NOT NULL,
+  `residence_phone_no` varchar(255) NOT NULL,
+  `employee` varchar(255) NOT NULL,
+  `designation` varchar(255) NOT NULL,
+  `official_address` varchar(255) NOT NULL,
+  `official_address_1` varchar(255) NOT NULL,
+  `official_address_2` varchar(255) NOT NULL,
+  `official_city` varchar(255) NOT NULL,
+  `official_district` varchar(255) NOT NULL,
+  `country` varchar(255) NOT NULL,
+  `enrolement_year_pim` varchar(255) NOT NULL,
+  `completion_year_pim` varchar(255) NOT NULL,
+  `year_of_graduation` varchar(255) NOT NULL,
+  `pim_student_no` varchar(255) NOT NULL,
+  `speciality` varchar(255) NOT NULL,
+  `experienced_industries` varchar(255) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `contact_number` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `img_url` text NOT NULL,
+  `flag` varchar(255) NOT NULL DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `member`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `member`
+--
+ALTER TABLE `member`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
